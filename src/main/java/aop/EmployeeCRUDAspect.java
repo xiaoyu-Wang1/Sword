29ad8543b644805deae8742c1ba032610d552841
yuquan.wang <yuquan.wang@qunar.com>
2019-06-25 22:50:26 +0800

    m

--- src/main/java/aop/EmployeeCRUDAspect.java
+++ src/main/java/aop/EmployeeCRUDAspect.java
@@ -3,8 +3,10 @@ package aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class EmployeeCRUDAspect {
     @Before("execution(* EmployeeManager.getEmployeeById(..))")         //point-cut expression
     public void logBeforeV1(JoinPoint joinPoint)
